fbf9e3bbba4de7cfeca94af2fb223b3da51a4cd8
Luigi Alberti <luigi.alberti@unipd.it>
2021-03-02 16:39:34 +0100

    Added line markers to add physical tags to lines

--- tests/test_regions.py
+++ tests/test_regions.py
@@ -8,16 +8,20 @@ pointBoundary =  [ (-1, -1),
                    ( 0, -1),
                    ( 1,  1),
                    ( 1, -3)]
+
 pointInner = [(0.5,0.5)]
 
 points = pointBoundary# + pointInner
 
 #segs = [(i, (i + 1) % len(pointBoundary)) for i in range(len(pointBoundary))]
 segs = [(0, 1),(1,2),(2,3),(3,0), (2,4), (4,5),(5,3)]
+_segs = [ 5,5,5,4,7,7,7]
 
 t = triangle.Triangle()
 t.set_points(points)
-t.set_segments(segs)
+t.set_segments(segs,_segs)
+#t.set_segment_markers(segs,_segs)
+print('ciao')
 
 # regions can be defined with a regional attribute 'r' at x,y coordinates.
 # Moreover it is possible to specify the area constraint in that region with
@@ -31,8 +35,11 @@ regions = [ (-0.5, 0.5,10,0.1),
 t.set_regions(regions)
 
 
-t.triangulate(mode='qpzAa')
+t.triangulate(mode='qpzAe',area=1)
 
 # a function to plot the triangulation, for fast check of regional attributes
 #  within the mesh triangles
 t.plot_mesh().show()
+
+#print(t.get_triangles())
+print(t.get_edges())
