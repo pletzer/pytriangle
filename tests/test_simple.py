9cf57ccba715a93c9c9e3a0455c30c81ae0ceae4
Alex Pletzer <alexander@gokliya.net>
2021-08-28 09:51:14 +1200

    fixing segments

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -5,10 +5,10 @@ def test_simple():
     # a single triangle
     t = triangle.Triangle()
     pts = [(0.,0.), (1.,0.), (1., 2.)]
-    seg = [(0,1), (1,2), (2,3)]
+    sgs = [(0,1), (1,2), (2,0),]
     t.set_points(pts)
-    t.set_segments(seg)
-    t.triangulate(area=0.01, mode='pzq27eQ')
+    t.set_segments(sgs)
+    t.triangulate(area=1.0, mode='pzq10eQ')
     print('number of points/triangles: %d/%d' %
         (t.get_num_points(), t.get_num_triangles()))
 
