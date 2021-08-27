b7302c72d35f8b357b5cb9859fdac1a1f0893cc4
Alex Pletzer <alexander@gokliya.net>
2021-08-28 09:33:38 +1200

    adding set_points and set_segments

---
+++ tests/test_simple.py
@@ -0,0 +1,16 @@
+import triangle
+
+def test_simple():
+
+    # a single triangle
+    t = triangle.Triangle()
+    pts = [(0.,0.), (1.,0.), (1., 2.)]
+    seg = [(0,1), (1,2), (2,3)]
+    t.set_points(pts)
+    t.set_segments(seg)
+    t.triangulate(area=0.01, mode='pzq27eQ')
+    print('number of points/triangles: %d/%d' %
+        (t.get_num_points(), t.get_num_triangles()))
+
+if __name__ == '__main__':
+    test_simple()
